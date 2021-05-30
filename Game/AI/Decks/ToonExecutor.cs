525e47868c21f6ef922f7dc71a7e974fd92b0496
dueltech <ygodueltech@gmail.com>
2021-05-29 17:05:36 -0700

    filling in more toon

--- Game/AI/Decks/ToonExecutor.cs
+++ Game/AI/Decks/ToonExecutor.cs
@@ -12,27 +12,15 @@ namespace WindBot.Game.AI.Decks
     {
         public class CardId
         {
-            public const int AbyssDweller = 21044178;
-            public const int BahamutShark = 440556;
-            public const int BorreloadDragon = 31833038;
-            public const int BorrelswordDragon = 85289965;
-            public const int ChimeratechMegafleetDragon = 87116928;
             public const int ComicHand = 33453260;
-            public const int DivineArsenalAAZEUSSkyThunder = 90448279;
-            public const int IPMasquerena = 65741786;
-            public const int KnightmareCerberus = 75452921;
-            public const int KnightmarePhoenix = 2857636;
-            public const int KnightmareUnicorn = 38342335;
             public const int Metaverse = 89208725;
             public const int Mimicat = 88032456;
-            public const int Number38HopeHarbingerDragonTitanicGalaxy = 63767246;
-            public const int Number70MalevolentSin = 80796456;
             public const int OneforOne = 2295440;
             public const int RedEyesToonDragon = 31733941;
             public const int Terraforming = 73628505;
             public const int TheBlackStoneofLegend = 66574418;
-            public const int ToadallyAwesome = 90809975;
             public const int ToonBookmark = 91500017;
+            public const int ToonBlackLusterSoldier = 28711704;
             public const int ToonBriefcase = 5832914;
             public const int ToonCyberDragon = 83629030;
             public const int ToonDarkMagician = 21296502;
@@ -43,8 +31,22 @@ namespace WindBot.Game.AI.Decks
             public const int ToonTableofContents = 89997728;
             public const int ToonTerror = 53094821;
             public const int ToonWorld = 15259704;
+
+            public const int ToadallyAwesome = 90809975;
             public const int TornadoDragon = 6983839;
+            public const int AbyssDweller = 21044178;
+            public const int BahamutShark = 440556;
+            public const int BorreloadDragon = 31833038;
+            public const int BorrelswordDragon = 85289965;
+            public const int ChimeratechMegafleetDragon = 87116928;
+            public const int DivineArsenalAAZEUSSkyThunder = 90448279;
+            public const int IPMasquerena = 65741786;
+            public const int KnightmareCerberus = 75452921;
+            public const int KnightmarePhoenix = 2857636;
+            public const int KnightmareUnicorn = 38342335;
             public const int UnderworldGoddessoftheClosedWorld = 98127546;
+            public const int Number38HopeHarbingerDragonTitanicGalaxy = 63767246;
+            public const int Number70MalevolentSin = 80796456;
         }
 
         public ToonExecutor(GameAI ai, Duel duel) : base(ai, duel)
@@ -59,8 +61,17 @@ namespace WindBot.Game.AI.Decks
             AddExecutor(ExecutorType.Activate, CardId.Terraforming, TerraformingEffect);
             AddExecutor(ExecutorType.Activate, CardId.ToonKingdom, FieldSpellActivate); // TODO check deck > 3?
             AddExecutor(ExecutorType.Activate, CardId.ToonWorld, ToonWorldEffect);
+            AddExecutor(ExecutorType.Activate, CardId.ToonBookmark, ToonBookmarkEffect);
+            AddExecutor(ExecutorType.Activate, CardId.Metaverse);
+
+            AddExecutor(ExecutorType.Activate, CardId.ToonBriefcase);
+
+            AddExecutor(ExecutorType.SpSummon, CardId.ToonCyberDragon);
+            AddExecutor(ExecutorType.SpSummon, CardId.ToonMermaid);
 
             AddExecutor(ExecutorType.Summon, CardId.TheBlackStoneofLegend);
+            AddExecutor(ExecutorType.SpSummon, CardId.ToonHarpieLady);
+            AddExecutor(ExecutorType.Summon, CardId.ToonHarpieLady);
 
             AddExecutor(ExecutorType.Activate, CardId.TheBlackStoneofLegend, TheBlackStoneofLegendEffect);
             AddExecutor(ExecutorType.Activate, CardId.RedEyesToonDragon, RedEyesToonDragonEffect);
@@ -88,10 +99,27 @@ namespace WindBot.Game.AI.Decks
 
         private bool FieldSpellActivate()
         {
+            Logger.WriteLine($"ToonKingdom {ActivateDescription}");
             //Do not replace own field spell!
             if (Bot.SpellZone[5] != null) {
                 return false;
             }
+            return true;
+        }
+
+        private bool ToonBookmarkEffect()
+        {
+            if (Bot.SpellZone[5] == null) {
+                AI.SelectCard(CardId.ToonKingdom);
+                return true;
+            }
+            AI.SelectCard(
+                        CardId.ToonPageFlip
+                        ,CardId.ToonBriefcase
+                        ,CardId.ToonBlackLusterSoldier
+                        ,CardId.ToonTerror
+                        ,CardId.ToonKingdom
+                    );
             return true;
         }
 
